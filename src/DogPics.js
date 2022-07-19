856effd252471b1b0e802a63a4d8e11879600ce9
Precious <Preshomondi@gmail.com>
2022-07-19 09:01:49 +0300

    commit tree de2ab3e9bdf9f8d74301e59141a6ff9f6bd042f9

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
 function DogPics() {
@@ -10,17 +11,17 @@ function DogPics() {
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
+        
         setImages(data.message);
       });
-  });
+  }, []);
 
   console.log("render");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt={""}/>
       ))}
     </div>
   );
